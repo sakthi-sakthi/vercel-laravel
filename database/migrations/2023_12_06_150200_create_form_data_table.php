e863b5cc0c08069d9fe85ad8d051fa246a4d80e4
sakthi-sakthi <sakthiganapathis97@gmail.com>
2023-12-07 09:47:31 +0530

    update
    update

--- database/migrations/2023_12_06_150200_create_form_data_table.php
+++ database/migrations/2023_12_06_150200_create_form_data_table.php
@@ -11,7 +11,6 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('form_data', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
             $table->string('email');
             $table->text('message');
